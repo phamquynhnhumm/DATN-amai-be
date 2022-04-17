b3379d2d2cfb374c42a735cf32b7d57dc0ac0c04
NhuPTQ <quynhnhuctcn@gmail.com>
2022-04-17 14:45:43 +0700

    update amai-be

---
+++ amain-be/src/test/java/com/example/amainbe/AmainBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amainbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmainBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
