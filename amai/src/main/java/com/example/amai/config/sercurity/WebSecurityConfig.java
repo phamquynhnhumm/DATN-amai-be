2132a6183d61c7c5e69ae1d307a34f913f903585
NhuPTQ <quynhnhuctcn@gmail.com>
2022-06-15 08:38:33 +0700

    update phan quen api/shop

--- amai/src/main/java/com/example/amai/config/sercurity/WebSecurityConfig.java
+++ amai/src/main/java/com/example/amai/config/sercurity/WebSecurityConfig.java
@@ -76,7 +76,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //              Phân quyền phía user
                 .antMatchers("/api/food/**").permitAll()
                 .antMatchers("/api/users/**").hasRole("CUSTOMER")
-                .antMatchers("/api/shop/**").hasRole("CUSTOMER")
+                .antMatchers("/api/shop/**").permitAll()
                 .antMatchers("/api/chat/**").hasRole("CUSTOMER")
                 .antMatchers("/api/sinup/**").permitAll()
                 .antMatchers("/api/forgot/**").permitAll()
