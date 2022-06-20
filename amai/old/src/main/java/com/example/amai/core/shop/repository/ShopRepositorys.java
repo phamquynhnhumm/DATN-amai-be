9cb3412b1b6dbcdf88ab5d6773b2b7eb44c4a933
NhuPTQ <quynhnhuctcn@gmail.com>
2022-06-20 16:48:41 +0700

    update deplpy

--- amai/old/src/main/java/com/example/amai/core/shop/repository/ShopRepositorys.java
+++
@@ -1,7 +0,0 @@
-package com.example.amai.core.shop.repository;
-
-import com.example.amai.core.shop.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepositorys extends JpaRepository<Shop, Integer> {
-}
